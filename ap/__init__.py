b9e072c5e7004fa8c8bb85e41f791bf9181bf4a5
Tom Strange <tpstrange@gmail.com>
2026-03-29 18:20:54 +0100

    Feature: Implement ternary averaged perceptron, where the weights are -1, - or +1 only.

--- ap/__init__.py
+++ ap/__init__.py
@@ -1,4 +1,5 @@
 from .easiestfirst.tagger import IngredientTaggerEasiestFirst  # noqa: F401
 from .greedy.tagger import IngredientTagger  # noqa: F401
 from .numpy.tagger import IngredientTaggerNumpy  # noqa: F401
+from .ternary.tagger import IngredientTaggerTernary  # noqa: F401
 from .viterbi.tagger import IngredientTaggerViterbi  # noqa: F401
